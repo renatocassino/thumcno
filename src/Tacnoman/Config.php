228336acc3e3a5283b32f2c41c79fbe6f73b4429
Tacnoman <renatocassino@gmail.com>
2016-11-05 12:29:20 -0200

    Testing new configuration

--- src/Tacnoman/Config.php
+++ src/Tacnoman/Config.php
@@ -8,20 +8,64 @@ namespace Tacnoman;
  */
 class Config {
     private static $_instance = null;
+    public $domain;
+    public $thumcnoPath;
+    public $host;
+    public $port;
     public $params = [];
     public $defines;
 
     protected function __construct() {
     }
 
-    public static function getInstance() {
+    public static function getInstance()
+    {
         if(is_null(self::$_instance)) {
             self::$_instance = new static();
         }
         return self::$_instance;
     }
 
-    public static function setConfiguration() {
-        
+    public function setConfiguration()
+    {
+        $this->setThumcnoPath();
+        $this->setDomain();
+        $this->setPort();
+        $this->setHost();
+    }
+
+    public function setThumcnoPath()
+    {
+        if(!isset($_ENV['THUMCNO_PATH'])) {
+            throw new \Exception('500', 'You must define the const `THUMCNO_PATH`.');
+        }
+        $this->thumcnoPath = $_ENV['THUMCNO_PATH'];
+    }
+
+    public function setDomain()
+    {
+        if (isset($_SERVER['HTTP_HOST'])) {
+            $domain = $_SERVER['HTTP_HOST'];
+            if (strstr($domain, ':')) {
+                $domain = explode(':', $domain)[0];
+            }
+        } else {
+            $domain = $_SERVER['SERVER_NAME'];
+        }
+
+        $this->domain = $domain;
+    }
+
+    public function setPort()
+    {
+        $this->port = $_SERVER['SERVER_PORT'];
+    }
+
+    public function setHost()
+    {
+        $this->host = "http://{$this->domain}";
+        if(80 != $this->port) {
+            $this->host .= ":{$this->port}";
+        }
     }
 }
--- tests/unit/ConfigurationTest.php
+++ tests/unit/ConfigurationTest.php
@@ -1,23 +1,23 @@
 <?php
 
-
 class ConfigurationTest extends \Codeception\Test\Unit
 {
     /**
      * @var \UnitTester
      */
     protected $tester;
+    protected $config;
 
     protected function _before()
     {
-        foreach(glob(dirname(dirname(__DIR__)) . '/src/Tacnoman/**.php') as $file) {
-            echo $file;
+        $rootPath = dirname(dirname(__DIR__));
+        require_once $rootPath . '/vendor/autoload.php';
+
+        foreach(glob($rootPath . '/src/Tacnoman/**.php') as $file) {
             require_once $file;
         }
-    }
 
-    protected function _after()
-    {
+        $this->config = \Tacnoman\Config::getInstance();
     }
 
     // tests
@@ -27,10 +27,58 @@ class ConfigurationTest extends \Codeception\Test\Unit
         $this->assertInstanceOf('\Tacnoman\Config', $config);
     }
 
-    #public function testSingletonError()
-    #{
-    #    $this->expectException(\Exception::class, function() {
-    #        new \Tacnoman\Config();
-    #    });
-    #}
+    public function testThumcnoPath()
+    {
+        $_ENV['THUMCNO_PATH'] = '/tmp/';
+        $this->config->setThumcnoPath();
+        $this->assertEquals($this->config->thumcnoPath, '/tmp/');
+    }
+
+    public function testDomainWithHttpHost()
+    {
+        $_SERVER['HTTP_HOST'] = 'myhost:8888';
+        $this->config->setDomain();
+        $this->assertEquals($this->config->domain, 'myhost');
+    }
+
+    public function testDomainWithHttpHostWithoutPort()
+    {
+        $_SERVER['HTTP_HOST'] = 'myhosts';
+        $this->config->setDomain();
+        $this->assertEquals($this->config->domain, 'myhosts');
+    }
+
+    public function testDomainWithoutHttpHost()
+    {
+        $_SERVER['SERVER_NAME'] = 'myhostname';
+        unset($_SERVER['HTTP_HOST']);
+        
+        $this->config->setDomain();
+        $this->assertEquals($this->config->domain, 'myhostname');
+    }
+
+    public function testPort()
+    {
+        $_SERVER['SERVER_PORT'] = 8888;
+        $this->config->setPort();
+        $this->assertEquals($this->config->port, 8888);
+    }
+
+    public function testHostnameWithDifferentPort()
+    {
+        $_ENV['THUMCNO_PATH'] = '/';
+        $_SERVER['SERVER_PORT'] = 8888;
+        $_SERVER['SERVER_NAME'] = 'myhostname.local';
+        $this->config->setConfiguration();
+        $this->assertEquals($this->config->host, 'http://myhostname.local:8888');
+    }
+
+    public function testHostnameWithDefaultPort()
+    {
+        $_ENV['THUMCNO_PATH'] = '/';
+        $_SERVER['SERVER_PORT'] = 80;
+        $_SERVER['SERVER_NAME'] = 'myhostname.local';
+        $this->config->setConfiguration();
+        $this->assertEquals($this->config->host, 'http://myhostname.local');
+    }
 }
